0dbe2a657f565eed301331c5da82b4acf459728c
LYFxiaoan <lyfxinchen@163.com>
2026-07-02 09:49:56 +0800

    feat: initialize Electron + Vite + Vue3 + TypeScript project structure

---
+++ electron.vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  main: {},
+  preload: {},
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src')
+      }
+    },
+    plugins: [vue()],
+    server: {
+      host: '127.0.0.1',
+      port: 5173
+    }
+  }
+})
